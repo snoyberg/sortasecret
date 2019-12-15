0f03353f6536493186661219735081078abd1a3b
Michael Snoyman <michael@snoyman.com>
2019-12-15 12:19:57 +0200

    Arguable improvement: MyServer struct

--- src/server.rs
+++ src/server.rs
@@ -9,28 +9,40 @@ use std::sync::Arc;
 use std::collections::HashMap;
 use askama::Template;
 
+struct MyServer {
+    script: String,
+    keypair: Keypair,
+    homepage: String,
+    recaptcha_secret: String,
+}
+
 pub fn run(settings: Server) -> Result<(), super::keypair::Error> {
-    let script: Arc<String> = Arc::new(make_script(&settings));
-    let keypair = Arc::new(Keypair::decode(&settings.keypair)?);
-    let homepage = Arc::new(make_homepage(&keypair));
-    let rs = Arc::new(settings.recaptcha_secret);
+    let keypair = Keypair::decode(&settings.keypair)?;
+    let homepage = make_homepage(&keypair);
+    let my_server = MyServer {
+        script: make_script(&settings),
+        keypair,
+        homepage,
+        recaptcha_secret: settings.recaptcha_secret,
+    };
+
+    let my_server = Arc::new(my_server);
 
     HttpServer::new(move || {
-        let keypair_pubkey = keypair.clone();
-        let keypair_encrypt = keypair.clone();
-        let keypair_decrypt = keypair.clone();
-        let keypair_show = keypair.clone();
-        let script = script.clone();
-        let homepage = homepage.clone();
-        let rs = rs.clone();
+        let my_server1 = my_server.clone();
+        let my_server2 = my_server.clone();
+        let my_server3 = my_server.clone();
+        let my_server4 = my_server.clone();
+        let my_server5 = my_server.clone();
+        let my_server6 = my_server.clone();
 
         App::new()
-            .route("/v1/pubkey", web::get().to(move || pubkey(keypair_pubkey.clone())))
-            .route("/v1/encrypt", web::get().to(move |encreq| encrypt(encreq, keypair_encrypt.clone())))
-            .route("/v1/decrypt", web::put().to(move |decreq| decrypt(decreq, keypair_decrypt.clone(), &rs)))
-            .route("/v1/script.js", web::get().to(move || script_js(script.clone())))
-            .route("/v1/show", web::get().to(move |encreq| show_html(encreq, keypair_show.clone())))
-            .route("/", web::get().to(move |_: HttpRequest| homepage_html(homepage.clone())))
+            .route("/v1/pubkey", web::get().to(move || my_server1.pubkey()))
+            .route("/v1/encrypt", web::get().to(move |encreq| my_server2.encrypt(encreq)))
+            .route("/v1/decrypt", web::put().to(move |decreq| my_server3.decrypt(decreq)))
+            .route("/v1/script.js", web::get().to(move || my_server4.script_js()))
+            .route("/v1/show", web::get().to(move |encreq| my_server5.show_html(encreq)))
+            .route("/", web::get().to(move |_: HttpRequest| my_server6.homepage_html()))
     })
         .bind(settings.bind)?
         .run()?;
@@ -38,8 +50,51 @@ pub fn run(settings: Server) -> Result<(), super::keypair::Error> {
     Ok(())
 }
 
-fn pubkey(keypair: Arc<Keypair>) -> HttpResponse {
-    HttpResponse::Ok().body(&keypair.public_hex)
+impl MyServer {
+    fn pubkey(&self) -> HttpResponse {
+        HttpResponse::Ok().body(&self.keypair.public_hex)
+    }
+
+    fn encrypt(&self, encreq: web::Query<EncryptRequest>) -> HttpResponse {
+        HttpResponse::Ok().body(self.keypair.encrypt(&encreq.secret))
+    }
+
+    fn decrypt(&self, decreq: web::Json<DecryptRequest>) -> HttpResponse {
+        // FIXME evil blocking
+        async_std::task::block_on(decrypt_async(self, decreq))
+    }
+
+    fn script_js(&self) -> HttpResponse {
+        HttpResponse::Ok()
+            .content_type("text/javascript; charset=utf-8")
+            .body(&self.script)
+    }
+
+    fn show_html(&self, encreq: web::Query<EncryptRequest>) -> HttpResponse {
+        // Check that the secret is actually valid. This also prevents an
+        // XSS attack, since only simple hex values will be allowed
+        // through.
+        match self.keypair.decrypt(&encreq.secret) {
+            Ok(_) => {
+                let html = ShowHtml {
+                    secret: &encreq.secret,
+                }.render().unwrap();
+                HttpResponse::Ok()
+                    .content_type("text/html; charset=utf-8")
+                    .body(&html)
+            }
+            Err(e) => {
+                eprintln!("{:?} {}", e, encreq.secret);
+                HttpResponse::NotFound().body("Invalid secret")
+            }
+        }
+    }
+
+    fn homepage_html(&self) -> HttpResponse {
+        HttpResponse::Ok()
+            .content_type("text/html; charset=utf-8")
+            .body(&self.homepage)
+    }
 }
 
 #[derive(Deserialize, Debug)]
@@ -88,10 +143,6 @@ struct DecryptResponse {
     decrypted: HashMap<String, String>,
 }
 
-fn encrypt(encreq: web::Query<EncryptRequest>, keypair: Arc<Keypair>) -> HttpResponse {
-    HttpResponse::Ok().body(keypair.encrypt(&encreq.secret))
-}
-
 async fn site_verify<'a>(body: &VerifyRequest<'a>) -> Result<VerifyResponse, VerifyError> {
     Ok(surf::post("https://www.google.com/recaptcha/api/siteverify")
         .set_header("User-Agent", "surf")
@@ -101,16 +152,11 @@ async fn site_verify<'a>(body: &VerifyRequest<'a>) -> Result<VerifyResponse, Ver
         .body_json().await?)
 }
 
-fn decrypt(decreq: web::Json<DecryptRequest>, keypair: Arc<Keypair>, recaptcha_secret: &str) -> HttpResponse {
-    // FIXME evil blocking
-    async_std::task::block_on(decrypt_async(decreq, keypair, recaptcha_secret))
-}
-
-async fn decrypt_async(decreq: web::Json<DecryptRequest>, keypair: Arc<Keypair>, recaptcha_secret: &str) -> HttpResponse {
+async fn decrypt_async(my_server: &MyServer, decreq: web::Json<DecryptRequest>) -> HttpResponse {
     let decreq = decreq.into_inner();
     let req = VerifyRequest {
         // Looks like a copy of this data is necessary, see https://serde.rs/feature-flags.html#-features-rc
-        secret: recaptcha_secret,
+        secret: &my_server.recaptcha_secret,
         response: decreq.token,
     };
     let secrets = decreq.secrets;
@@ -128,7 +174,7 @@ async fn decrypt_async(decreq: web::Json<DecryptRequest>, keypair: Arc<Keypair>,
                 let decrypted = secrets
                     .into_iter()
                     .map(|secret| {
-                        let cleartext = match keypair.decrypt(&secret) {
+                        let cleartext = match my_server.keypair.decrypt(&secret) {
                             Err(e) => format!("Could not decrypt secret: {:?}", e),
                             Ok(vec) => match String::from_utf8(vec) {
                                 Ok(s) => s,
@@ -146,42 +192,10 @@ async fn decrypt_async(decreq: web::Json<DecryptRequest>, keypair: Arc<Keypair>,
     }
 }
 
-fn script_js(body: Arc<String>) -> HttpResponse {
-    HttpResponse::Ok()
-        .content_type("text/javascript; charset=utf-8")
-        .body(&*body)
-}
-
-fn homepage_html(body: Arc<String>) -> HttpResponse {
-    HttpResponse::Ok()
-        .content_type("text/html; charset=utf-8")
-        .body(&*body)
-}
-
 #[derive(Template)]
 #[template(path = "show-html.html")]
 struct ShowHtml<'a> {
     secret: &'a str,
-}
-
-fn show_html(encreq: web::Query<EncryptRequest>, keypair: Arc<Keypair>) -> HttpResponse {
-    // Check that the secret is actually valid. This also prevents an
-    // XSS attack, since only simple hex values will be allowed
-    // through.
-    match keypair.decrypt(&encreq.secret) {
-        Ok(_) => {
-            let html = ShowHtml {
-                secret: &encreq.secret,
-            }.render().unwrap();
-            HttpResponse::Ok()
-                .content_type("text/html; charset=utf-8")
-                .body(&html)
-        }
-        Err(e) => {
-            eprintln!("{:?} {}", e, encreq.secret);
-            HttpResponse::NotFound().body("Invalid secret")
-        }
-    }
 }
 
 #[derive(Template)]
